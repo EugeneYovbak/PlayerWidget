81402c032728ffc0d46076f5f8f04ad5dd4b5e40
PerSpiKyliaTor <perspikyliator@gmail.com>
2018-06-11 19:57:42 +0300

    fixed permission issues

--- app/src/main/java/com/example/perspikyliator/playerwidget/presentation/MainActivity.java
+++ app/src/main/java/com/example/perspikyliator/playerwidget/presentation/MainActivity.java
@@ -6,54 +6,52 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Toast;
 
 import com.example.perspikyliator.playerwidget.R;
 import com.example.perspikyliator.playerwidget.presentation.service.player_widget.PlayerWidgetService;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
 
-    //TODO fix this shit
-
-    private static final int CODE_DRAW_OVER_OTHER_APP_PERMISSION = 2084;
+    private static final int REQUEST_DRAW_OVER_OTHER_APP_PERMISSION = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initializeView();
+        ButterKnife.bind(this);
     }
 
-    private void initializeView() {
-        findViewById(R.id.tv_title).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(MainActivity.this)) {
-                    Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                            Uri.parse("package:" + getPackageName()));
-                    startActivityForResult(intent, CODE_DRAW_OVER_OTHER_APP_PERMISSION);
-                } else {
-                    startService(new Intent(MainActivity.this, PlayerWidgetService.class));
-                    finish();
-                }
+    @OnClick(R.id.btn_play)
+    void onPlayClick() {
+        if (needToCheckPermission()) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, REQUEST_DRAW_OVER_OTHER_APP_PERMISSION);
+        } else {
+            startPlayer();
+        }
+    }
 
-            }
-        });
+    private boolean needToCheckPermission() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(MainActivity.this);
+    }
+
+    private void startPlayer() {
+        startService(new Intent(MainActivity.this, PlayerWidgetService.class));
+        finish();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == CODE_DRAW_OVER_OTHER_APP_PERMISSION) {
-            //Check if the permission is granted or not.
-            if (resultCode == RESULT_OK) {
-                initializeView();
-            } else { //Permission is not available
-                Toast.makeText(this,
-                        "Draw over other app permission not available. Closing the application",
-                        Toast.LENGTH_SHORT).show();
-
-                finish();
+        if (requestCode == REQUEST_DRAW_OVER_OTHER_APP_PERMISSION) {
+            if (needToCheckPermission()) {
+                Toast.makeText(this, getString(R.string.error_permission_denied), Toast.LENGTH_SHORT).show();
+            } else {
+                startPlayer();
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
